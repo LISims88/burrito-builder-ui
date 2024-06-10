d3c570b7f4cf03f0ca3878f77b6aa64a0cbb1f39
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-06-10 13:05:29 -0500

    Write a test to check that orders cannot be submitted without a name and at least one ingredient.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -63,4 +63,38 @@ describe("Burrito Builder", () => {
     cy.get('section')
     cy.get('section > :nth-child(4)')    
   })
+  it('nothing should happen when no name is added ', () =>{
+    cy.get('input').type('Lydia')
+    cy.get('[name="steak"]').click()
+    cy.get('p').contains('Order: steak')
+    cy.get('[name="queso fresco"]').click()
+    cy.get('p').contains('Order: steak, queso fresco')
+    cy.get('[name="pico de gallo"]').click()
+    cy.get('p').contains('Order: steak, queso fresco, pico de gallo')
+    cy.get(':nth-child(15)').click()
+  })
+});
+describe("sad path", () => {
+  beforeEach(()=>{
+    cy.intercept("GET", "http://localhost:3001/api/v1/orders", {
+      statusCode: 200,
+      fixture: 'example.json'
+    }).as('burritos');
+    cy.visit('http://localhost:3000')
+  })
+
+  it('nothing should happen when no name is added ', () =>{
+    cy.get('[name="steak"]').click()
+    cy.get('p').contains('Order: steak')
+    cy.get('[name="queso fresco"]').click()
+    cy.get('p').contains('Order: steak, queso fresco')
+    cy.get('[name="pico de gallo"]').click()
+    cy.get('p').contains('Order: steak, queso fresco, pico de gallo')
+    cy.get(':nth-child(15)').click()
+  })
+  it('nothing should happen when no ingredients are added  is added ', () =>{
+    cy.get('input').type('Lydia')
+    cy.get(':nth-child(15)').click()
+  })
+
 });
