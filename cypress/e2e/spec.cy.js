f19a26f660c7da77cbe8dc2cc94bc6d0171ce97d
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-06-10 12:57:30 -0500

    Write a test to check the user flow of adding a new order to the DOM

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,7 +3,7 @@ describe("Visiting Burrito Builder", () => {
     cy.visit("http://localhost:3000/");
   });
 });
-describe("Burrito Builder", () => {
+describe("Burrito Builder Ordering", () => {
   beforeEach(()=>{
     cy.intercept("GET", "http://localhost:3001/api/v1/orders", {
       statusCode: 200,
@@ -19,12 +19,48 @@ describe("Burrito Builder", () => {
     cy.get('input')
     cy.get('p').contains('Order: Nothing selected')
     cy.get(':nth-child(15)').contains('Submit Order')
-
   })
   it('shows the orders', () => {
     cy.get('section')
     cy.get('section > :nth-child(1)')
     cy.get('section > :nth-child(2)')
     cy.get('section > :nth-child(3)')
+  })
+});
+describe("Burrito Builder", () => {
+  beforeEach(()=>{
+    cy.intercept("GET", "http://localhost:3001/api/v1/orders", {
+      statusCode: 200,
+      fixture: 'example.json'
+    }).as('burritos');
+    cy.visit('http://localhost:3000')
+  })
+  it('fills out an order', () => {
+    cy.get('input').type('Lydia')
+    cy.get('[name="steak"]').click()
+    cy.get('p').contains('Order: steak')
+    cy.get('[name="queso fresco"]').click()
+    cy.get('p').contains('Order: steak, queso fresco')
+    cy.get('[name="pico de gallo"]').click()
+    cy.get('p').contains('Order: steak, queso fresco, pico de gallo')
+  })
+  it('should be able to submit an order', () =>{
+    cy.intercept('POST', "http://localhost:3001/api/v1/orders", {
+      statusCode: 200,
+      body: {
+        "name":"Lydia",
+        "ingredients": ["steak", "pico de gallo", "queso fresco"]
+      }
+    })
+    cy.get('input').type('Lydia')
+    cy.get('[name="steak"]').click()
+    cy.get('p').contains('Order: steak')
+    cy.get('[name="queso fresco"]').click()
+    cy.get('p').contains('Order: steak, queso fresco')
+    cy.get('[name="pico de gallo"]').click()
+    cy.get('p').contains('Order: steak, queso fresco, pico de gallo')
+    cy.get(':nth-child(15)').click()
+    cy.get('section')
+    cy.get('section > :nth-child(4)')    
   })
 });
