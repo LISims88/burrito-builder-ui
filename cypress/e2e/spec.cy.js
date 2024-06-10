156e903f6d0c4104de87ab5719d554b18a22a904
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-06-10 12:38:30 -0500

    Write tests covering what should be displayed when the user first visits the page

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,30 @@
-describe("empty spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
+describe("Visiting Burrito Builder", () => {
+  it("Shows Burrito Builder", () => {
+    cy.visit("http://localhost:3000/");
   });
+});
+describe("Burrito Builder", () => {
+  beforeEach(()=>{
+    cy.intercept("GET", "http://localhost:3001/api/v1/orders", {
+      statusCode: 200,
+      fixture: 'example.json'
+    }).as('burritos');
+    cy.visit('http://localhost:3000')
+  })
+  it('shows the header', () => {
+    cy.get('header').contains('h1', 'Burrito Builder')
+  })
+  it('shows the header form', () => {
+    cy.get('form')
+    cy.get('input')
+    cy.get('p').contains('Order: Nothing selected')
+    cy.get(':nth-child(15)').contains('Submit Order')
+
+  })
+  it('shows the orders', () => {
+    cy.get('section')
+    cy.get('section > :nth-child(1)')
+    cy.get('section > :nth-child(2)')
+    cy.get('section > :nth-child(3)')
+  })
 });
